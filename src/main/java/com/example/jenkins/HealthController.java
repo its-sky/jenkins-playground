bb05148fa35e93c1d233de5417771afe8dbbdf8f
its-sky <smc9919@naver.com>
2024-06-23 02:56:04 +0900

    feat: Health Check Controller 추가

---
+++ src/main/java/com/example/jenkins/HealthController.java
@@ -0,0 +1,15 @@
+package com.example.jenkins;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealth() {
+        return ResponseEntity.ok().body("UP");
+    }
+
+}
